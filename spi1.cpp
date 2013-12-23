c8c0960b1c0b07baf4ea1a29947b7235db5f2a67
Diego Rondelli <diego-ronde@hotmail.it>
2013-12-23 17:56:32 +0100

    Creato file spi1.cpp

---
+++ spi1.cpp
@@ -0,0 +1,33 @@
+/**
+ * 
+ * \author Diego Rondelli
+ * 
+ */
+#include <cstdio>
+#include "miosix.h"
+#include <unistd.h>
+
+using namespace std;
+using namespace miosix;
+
+typedef Gpio<GPIOB_BASE, 5> SCK;
+typedef Gpio<GPIOB_BASE, 6> MISO;
+typedef Gpio<GPIOB_BASE, 7> MOSI;
+
+/**
+ * This function sets up all the GPIO used for the accelerometer.
+ */
+
+void spiConfig()
+{
+    SCK::mode(Mode::ALTERNATE);
+    SCK::alternateFunction(0);
+    MISO::mode(Mode::ALTERNATE);
+    MISO::alternateFunction(0);
+    MOSI::mode(Mode::ALTERNATE);
+    MOSI::alternateFunction(0);
+    //TODO config PE3
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+}
+
+
