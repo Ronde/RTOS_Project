4e69e8de01cac60cf067ca7ff3f263f7dfb7d5d5
Omar Scotti <omar.scotti@mail.polimi.it>
2014-01-13 14:45:18 +0100

    Commit Stesura funzioni per ricezione dati da accellerometro tramite spi
    Aggiunta registri negli header
    
    Signed-off-by: Omar Scotti <omar.scotti@mail.polimi.it>

--- lis302dl.cpp
+++ lis302dl.cpp
@@ -1,42 +1,47 @@
-/**
- *\author Omar Scotti
- * 
+/* 
+ * File:   lis302dl.cpp
+ * Author: Omar Scotti
+ *
+ * Created on 13 gennaio 2014, 14.00
  */
-#include <cstdio>
-#include <unistd.h>
 #include "miosix.h"
 #include "lis302dl_reg.h"
-        
+#include <stdint.h>
+
 using namespace std;
 using namespace miosix;
-/**
- * Start function that set the communication
- */
-public void start(){
-    accelerometerConfig();
-    
-}
+
 /*
  * Private function that set the accelerometer registers
  */
-private void accellerometerConfig(){
+protected void accellerometerConfig(){
+    uint8_t val;
+    spiConfig();
     CTRL_REG1 |=( CTRL_REG1_XEN | CTRL_REG1_YEN  | CTRL_REG1_ZEN | CTRL_REG1_FSEN
             | CTRL_REG1_PDEN)
+    spiWrite(CTRL_REG1, &val, 1);
+
 }
 /*
  * 
  */
-private int getAcc(int16_t* x, int16_t* y, int16_t* z)
+protected void getAccelerometerData(int16_t* x, int16_t* y, int16_t* z)
 {
 	uint8_t buffer[6];
 	
 	/* Read out all 6 bytes in one shot */
-	spiRead(OUT_X, buffer, 6);
-	*x = *((int16_t*)buffer);
-	*y = *((int16_t*)(buffer + 2));
-	*z = *((int16_t*)(buffer + 4));
-	return 0;
+	*x = spiRead(OUT_X, 1);
+        *y = spiRead(OUT_Y, 1);
+        *z = spiRead(OUT_Z, 1);
 }
-
-
-
+/*
+void setRange(int8_t range)
+{
+	uint8_t regval;
+	assert_param(range == RANGE_2G || range == RANGE_4G || range == RANGE_8G || range == RANGE_16G);
+	regval = spiRead(CTRL_REG4, &regval, 1);
+	regval &= ~(3 << 4);
+	regval |= range;
+	spiWrite(CTRL_REG4, &regval, 1);
+}
+*/
