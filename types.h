ba5e8b2de0b2efacf4a575ec783a4756250c3f4b
Omar Scotti <omar.scotti@mail.polimi.it>
2014-01-15 15:26:16 +0100

    Creazione tipi specifici
    Signed-off-by: Omar Scotti <omar.scotti@mail.polimi.it>

---
+++ types.h
@@ -0,0 +1,15 @@
+/* 
+ * File:   types.h
+ * Author: user
+ *
+ * Created on 15 gennaio 2014, 10.44
+ */
+
+#ifndef TYPES_H
+#define	TYPES_H
+typedef unsigned          char uint8_t;
+typedef unsigned short     int uint16_t;
+typedef   signed short     int int16_t;
+
+#endif	/* TYPES_H */
+
