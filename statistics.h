64dfd6adbf88ba9efc3862ae522fcaf07a1d3194
Diego Rondelli <diego-ronde@hotmail.it>
2014-01-21 17:35:13 +0100

    Add statistics
    signed by diego rondelli

---
+++ statistics.h
@@ -0,0 +1,53 @@
+/**
+ * File:   pedometer.h
+ *  \brief     This file is the header file for the class statistics
+ * 
+ *  \author    Omar Scotti
+ *  \author    Diego Rondelli
+ *  \version   1.0
+ *  \date      21/01/2014
+ */
+
+#ifndef STATISTICS_H
+#define	STATISTICS_H
+class Statistics{
+public:
+    
+    static Statistics* getInstance();
+    
+    void start();
+    
+    float getAvg();
+    
+    float getCal();
+    
+    int getTime();
+    
+    float getDistance();
+    
+        ~Pedometer()
+    {
+        instanceFlag = false;
+    }
+
+private:
+    
+    Statistics();
+    
+    static Statistics* statistics;
+    static bool instanceFlag;
+    float avg;
+    int oldSteps;
+    float distance;
+    int seconds;
+    int stepsInInterval;
+    int height;
+    float multParam;
+    bool moving;
+           
+};
+
+
+
+#endif	/* STATISTICS_H */
+
